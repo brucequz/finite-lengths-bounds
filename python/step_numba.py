b8dc1450dc0f2214880148b7f57ffe97167e8aa5
brucequz <brucequ@ucla.edu>
2026-04-03 17:02:58 -0700

    feat: fold shift pass k11n22v3 test

--- python/step_numba.py
+++ python/step_numba.py
@@ -131,4 +131,82 @@ def numba_sharedMem_trellisStep_foldshift(A_in, A_shape, W_in, D_in, out):
 
     """
     # TODO: implement in NUMBA
-    pass
+    num_states = A_shape[0]
+    curr_max_weight = A_shape[1]
+
+    x, y, z = cuda.grid(3)
+    bx, by, bz = cuda.blockIdx.x, cuda.blockIdx.y, cuda.blockIdx.z
+    bs_x, bs_y, bs_z = cuda.blockDim.x, cuda.blockDim.y, cuda.blockDim.z
+    tx, ty, tz = cuda.threadIdx.x, cuda.threadIdx.y, cuda.threadIdx.z
+    W_shape, D_shape = W_in.shape, D_in.shape
+    mid_y = num_states // 2
+
+    ## Load W into shared memory
+    # we process 64 states for each thread block
+    # storage requirement: 64*2*1 = 128 bytes
+    shared_W = cuda.shared.array(
+        shape=(32, 2), dtype=np.uint8
+    )  # 1 bytes for weight, support up to weight=255
+    if y < mid_y and z < W_shape[1]:
+        shared_W[ty, tz] = W_in[y, z]
+        shared_W[ty + bs_y, tz] = W_in[y + mid_y, z]
+
+    ## Load D into shared memory
+    # we process 64 states for each thread block
+    # storage requirement: 64*2*4 = 512 bytes
+    shared_D = cuda.shared.array(
+        shape=(32, 2), dtype=np.uint32
+    )  # 32 bits to support states up to 2^32-1
+    if y < mid_y and z < D_shape[1]:
+        shared_D[ty, tz] = D_in[y, z]
+        shared_D[ty + bs_y, tz] = D_in[y + mid_y, z]
+
+    ## Load A into shared memory
+    # storage requirement: 64*32*8 = 16,384 or 64*32*16 = 32,768
+    shared_A = cuda.shared.array(shape=(32, 32), dtype=np.uint64)  # 64 bits or 128 bits
+    if y < mid_y and x < A_shape[1]:
+        shared_A[ty, tx] = A_in[y, x]
+        shared_A[ty + bs_y, tx] = A_in[y + mid_y, x]
+
+    ## Allocate space for output in shared memory
+    shared_out = cuda.shared.array(shape=(32, 34), dtype=np.uint64)
+    if y < mid_y and x < A_shape[1]:
+        shared_out[ty, tx] = 0
+        if x + 32 < A_shape[1] + 2:
+            shared_out[ty + bs_y, tx] = 0
+
+    cuda.syncthreads()
+
+    dst_state_offset = shared_D[0, 0]
+    if x == 0 and y == 0 and z == 0:
+        print("dst_state_offset: ", dst_state_offset)
+
+    ## first group of states shift and write to result
+    if y < mid_y and x < A_shape[1]:
+        shift_amt_0 = shared_W[ty, tz]
+        dst_state_0 = shared_D[ty, tz]
+        shifted_x_0 = x + shift_amt_0
+        shared_out[dst_state_0, shifted_x_0] += shared_A[ty, tx]
+        if y == 0 and x == 0 and z == 1:
+            print("shared_A[ty, tx]: ", shared_A[ty, tx])
+            print("W_in[y, z]: ", W_in[y, z])
+            print("shared_W[ty, tz]: ", shared_W[ty, tz])
+            print("dst_state_0: ", dst_state_0)
+            print("shifted_x_0: ", shifted_x_0)
+            print(
+                "shared_out[dst_state_0, shifted_x_0]",
+                shared_out[dst_state_0, shifted_x_0],
+            )
+
+    ## second group of states shift and write to result
+    if y < mid_y and x < A_shape[1]:
+        shift_amt_1 = shared_W[ty + bs_y, tz]
+        dst_state_1 = shared_D[ty + bs_y, tz]
+        shifted_x_1 = x + shift_amt_1
+        shared_out[dst_state_1, shifted_x_1] += shared_A[ty + bs_y, tx]
+
+    ## Write shared_out to global memory
+    # To avoid atomic operations, each threadblock needs to process
+    # the entire row of A_in
+    if y < A_shape[0] and x < A_shape[1] + 2 and z == 0:
+        out[y, x] += shared_out[ty, tx]
--- python/trellisStep_foldshift_test.py
+++ python/trellisStep_foldshift_test.py
@@ -1,14 +1,17 @@
 import numpy as np
 import yaml
+import math
+from numba import cuda
 from setup import setup_A_Wbit_D
 from step import trellisStep_shift, trellisStep_folded_shift
+from step_numba import numba_sharedMem_trellisStep_foldshift
 
 import sys
 
 
 def main():
 
-    path = "config/k51n126v6.yaml"
+    path = "config/k11n22v3.yaml"
     try:
         with open(path, "r") as f:
             code_config = yaml.safe_load(f)
@@ -19,21 +22,69 @@ def main():
     output_file_name = code_config["output_file_name"]
 
     As, W_weight, D, basis, num_trellis_stages = setup_A_Wbit_D(path)
-    A = As[5]
+    A = As[0]
+    W_y, W_z = W_weight.shape
+    A_shape = A.shape
+    O_y, O_x = A_shape
+    print("W_weight: ", W_weight)
 
     ## Ref
     # a single trellis Step
     ref_result = trellisStep_shift(A, W_weight, D, 2)
 
     # DUT
-    dut_result = trellisStep_folded_shift(A, W_weight, D, 2)
+    num_stages = 1
+    max_shift_per_stage = 2
+
+    # prepare ping-pong buffer at CPU
+    max_X = A_shape[1] + (max_shift_per_stage) * num_stages
+    h_in_buffer = np.zeros(shape=(O_y, max_X), dtype=np.float64)
+    h_in_buffer[0 : A.shape[0], 0 : A.shape[1]] = A
+    h_out_buffer = np.zeros(shape=(O_y, max_X), dtype=np.float64)
+
+    # move ping-pong buffers to GPU
+    d_buffer_in = cuda.to_device(h_in_buffer)
+    d_buffer_out = cuda.to_device(h_out_buffer)
+    d_buffer_allzero = cuda.to_device(h_out_buffer)
+
+    # moving W and D to GPU
+    d_W = cuda.to_device(W_weight)
+    d_D = cuda.to_device(D)
+
+    # block and grid size allocation
+    threads_per_block = (32, 16, 2)
+
+    for i_stage in range(num_stages):
+        O_x += max_shift_per_stage
+
+        grid_x = math.ceil(O_x / threads_per_block[0])
+        grid_y = math.ceil((O_y // 2) / threads_per_block[1])
+        grid_z = math.ceil(W_z / threads_per_block[2])
+        blocks_per_grid = (grid_x, grid_y, grid_z)
+        print("blocks_per_grid: ", blocks_per_grid)
+
+        # Zero the entire max buffer
+        d_buffer_out.copy_to_device(d_buffer_allzero)
+
+        numba_sharedMem_trellisStep_foldshift[blocks_per_grid, threads_per_block](
+            d_buffer_in, A_shape, d_W, d_D, d_buffer_out
+        )
+
+        d_buffer_in, d_buffer_out = d_buffer_out, d_buffer_in
+
+        A_shape = tuple((A_shape[0], O_x))
+
+    dut_result = d_buffer_in.copy_to_host()
 
     assert ref_result.shape == dut_result.shape
-    print("ref_result: ", ref_result)
-    print("dut_result: ", dut_result)
+    print("ref_result shape: ", ref_result.shape)
+    print("dut_result shape: ", dut_result.shape)
 
     if np.allclose(ref_result, dut_result):
         print("Success!")
+    else:
+        print("ref_result: ", ref_result)
+        print("dut_result: ", dut_result)
 
 
 if __name__ == "__main__":
